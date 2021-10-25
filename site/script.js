a261e001f3babda54efa43c9adbf99afd89c4505
Nhi <68318731+QuynhNgocNhi@users.noreply.github.com>
2021-10-25 15:21:18 +0700

    update script.js

--- site/script.js
+++ site/script.js
@@ -12,11 +12,11 @@ function Hinhtron (bankinh){
 var circle = new Hinhtron(5);
 console.log(circle);
 */
-function Cat(name){
-  this.name=name;
-}
-Cat.prototype.meow= function(){
-  console.log(this.name + " says chuc ban may man lan sau!");
-}
-var Ngheo = new Cat("Ngheo", "Kho");
-Ngheo.meow();
+// function Cat(name){
+//   this.name=name;
+// }
+// Cat.prototype.meow= function(){
+//   console.log(this.name + " says chuc ban may man lan sau!");
+// }
+// var Ngheo = new Cat("Ngheo", "Kho");
+// Ngheo.meow();
